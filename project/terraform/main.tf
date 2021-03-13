a10100b4e30813a9c19aa8e21cc5b58e1ab61a94
alex-kay <ichijoint@gmail.com>
2021-03-13 03:55:39 +0200

    additions to playbooks

--- project/terraform/main.tf
+++ project/terraform/main.tf
@@ -20,13 +20,13 @@ resource "aws_instance" "jenkins-main" {
   user_data = <<-EOF
     #!/bin/bash
     echo ${var.KEY_ANSIBLE} >> /home/ec2-user/.ssh/authorized_keys
-    sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat/jenkins.repo
-    sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
-    sudo yum upgrade
-    sudo yum install jenkins java-1.8.0-openjdk-devel git -y
-    sudo systemctl daemon-reload
-    sudo systemctl start jenkins
+    # sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    # https://pkg.jenkins.io/redhat/jenkins.repo
+    # sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+    # sudo yum upgrade
+    # sudo yum install jenkins java-1.8.0-openjdk-devel git -y
+    # sudo systemctl daemon-reload
+    # sudo systemctl start jenkins
     EOF
   tags = {
     Name = "JenkinsMain"
